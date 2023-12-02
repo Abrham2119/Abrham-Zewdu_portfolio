5265ab493224231e1a711e640b088b13bd30d032
Abrham2119 <abrhamzewdu2119@gmail.com>
2023-12-02 09:48:43 +0300

    i changed the style of right section of the service section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import Navbar from "./component/Navbar/Navbar";
-import './App.css'
+import './App.css';
 import Intro from "./component/Intro/Intro";
 import FloatingDiv from "./component/FloatingDiv/FloatingDiv";
+import Service from './component/Service/Service';  // Adjust the path accordingly
 
 function App() {
-  return (
+  return(
     <div className="App">
       <Navbar />
       <Intro /> 
--- src/component/Service/Service.jsx
+++ src/component/Service/Service.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import './Service..css'
+import './Service.css'
+import heartemoji from '../../img/heartemoji.png'
+import glasses from '../../img/glasses.png'
+import Humble from '../../img/humble.png'
+
 
 const Service = () => {
   return (
-    <div>Service</div>
+    <div className="service">
+
+      {/* l-side */}
+      <div className="awsome">
+        <span>My Awsome</span>
+        <span>Service</span>
+        <span>Lorem Ipsum is simply dummy text of the <br /> printing and  typesetting industry. </span>
+        <button className="button s-button">Download CV</button>
+      </div>
+      
+      
+      {/* blur section */}
+      <div className="blur s-blur" style={{background: "#ABF!FF94"}}>
+
+      </div>
+
+
+      {/* r-side */}
+
+      <div className="cards">
+        this is the right side
+      </div>
+    </div>
   )
 }
 
