867f988ed3c20c5195716b14b8da760076d3696c
Abrham2119 <abrhamzewdu2119@gmail.com>
2023-12-03 05:08:23 +0300

    Added the blur style to service.css

--- src/component/Service/Service.jsx
+++ src/component/Service/Service.jsx
@@ -17,45 +17,47 @@ const Service = () => {
         <span>Service</span>
         <span>Lorem Ipsum is simply dummy text of the <br /> printing and  typesetting industry. </span>
         <a href={AbrhamResume} download>
-        <button className="button s-button">Download CV</button>
+          <button className="button s-button">Download CV</button>
         </a>
       </div>
-      
-      
-      {/* blur section */}
-      <div className="blur s-blur" style={{background: "var(--purple)"}}>
 
-      </div>
 
-      
+
+
 
       {/* r-side */}
 
-      <div className="cards"  style={{left:'14rem'}}>
-         <Card
+      <div className="cards" style={{ left: '14rem' }}>
+        <Card
           emoji={heartemoji}
           heading={'Design'}
           detail={"Figma, Skech ,Photoshop ,Adobe , Adobe xd"}
-          />
+        />
       </div>
-      <div className="cards"  style={{left:'-4rem', top:'12rem' }}>
-         <Card
+      <div className="cards" style={{ left: '-4rem', top: '12rem' }}>
+        <Card
           emoji={glasses}
           heading={'Developer'}
           detail={"Html, CSS ,JavaScript ,React"}
-          />
+        />
       </div>
-      <div className="cards"  style={{left:'12rem', top:'19rem' }}>
-         <Card
+      <div className="cards" style={{ left: '12rem', top: '19rem' }}>
+        <Card
           emoji={Humble}
           heading={"UX/UI"}
           detail={"Html, CSS ,JavaScript ,React"}
-          />
+        />
       </div>
 
       {/* service blur section  */}
 
-      
+      <div className="blur s-blur1" style={{ background: "var(--purple)" }}>
+
+      </div>
+      <div className="blur s-blur2" style={{ background: '#c1f5ff' }}>
+
+      </div>
+
 
 
 
