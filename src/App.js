99f3d42ab7bfe91ac743723ceeca14f530da7296
Abrham2119 <abrhamzewdu2119@gmail.com>
2023-12-04 18:08:15 +0300

    added portfolio components

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import Intro from "./component/Intro/Intro";
 import FloatingDiv from "./component/FloatingDiv/FloatingDiv";
 import Service from './component/Service/Service';  // Adjust the path accordingly
 import Experience from './component/Experience/Experience'
+import Works from './component/Works/Works';
+import Portfolio from './component/Portfolio/Portfolio';
 
 function App() {
   return(
-    <div className="App">
+    <div className="App" style={{background:'var(black)'}}>
       <Navbar />
       <Intro /> 
       <Service/>
-      <Experience/>      
+      <Experience/>  
+      <Works/>
+      <Portfolio/>
+
     </div>
   );
 }
---
+++ src/component/Works/Works.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import './Works.css'
+import Upwork from '../../img/Upwork.png'
+import Fiver from'../../img/fiverr.png'
+import Amazon from '../../img/amazon.png'
+import Shopify from '../../img/Shopify.png'
+import Facebook from '../../img/Facebook.png'
+
+
+const Works = () => {
+  return (
+    <div className="works">
+      <div className="awsome">
+        <span>Works of all goal</span>
+        <span>Brands & Clients</span>
+        <span>Lorem Ipsum is simply dummy text of the
+          <br /> printing and  typesetting industry. <br />
+          Lorem Ipsum is simply dummy text of the   <br />
+          printing and  typesetting industry.
+        </span>
+
+
+        <button className="button s-button">Hire me</button>
+        <div className="blur s-blur2" style={{ background: '#c1f5ff' }}>
+
+        </div>
+      </div>
+
+
+      {/* right side works */}
+      <div className="w-right">
+        <div className="w-mainCircle">
+          <div className="w-secCircle">
+            <img src={Upwork} alt="" srcset="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Fiver} alt="" srcset="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Amazon} alt="" srcset="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Shopify} alt="" srcset="" />
+          </div>
+          <div className="w-secCircle">
+            <img src={Facebook} alt="" srcset="" />
+          </div>
+          {/* background Circle */}
+
+         <div>
+         <div className="backCircle blueCircle"></div>
+         <div className="backCircle yellowCircle"></div>
+  
+         </div>
+
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Works
