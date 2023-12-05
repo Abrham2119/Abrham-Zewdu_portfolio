f4d4d89946891bbf29bc403e7fc027393bbe6783
Abrham2119 <abrhamzewdu2119@gmail.com>
2023-12-05 13:00:42 +0300

    changed the jsx of  portfolio component

--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './Portfolio.css'
+import {Swiper, SwiperSlide} from 'swiper/react'
+import HOC from '../../img/hoc.png'
+import MusicApp from '../../img/musicapp.png'
+import Slidbar from '../../img/sidebar.png'
+import Ecommerce from '../../img/ecommerce.png'
+import 'swiper/css'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className="portfolio">
+      {/* heading for section */}
+      <span>Recent Projects</span>
+      <span>Portfolio</span>
+
+      {/* slider */}
+      <Swiper>
+        <SwiperSlide>
+          <img src={Slidbar} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={MusicApp} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Ecommerce} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={HOC} alt="" />
+        </SwiperSlide>
+      </Swiper>
+
+    </div>
   )
 }
 
