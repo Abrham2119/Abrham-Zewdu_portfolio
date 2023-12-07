d767b10bd1b1842eb9d73122c1b77b8d6a6f1a32
Abrham2119 <abrhamzewdu2119@gmail.com>
2023-12-07 11:33:29 +0300

    added the footer file and  style

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Footer.css'
 import wave from '../../img/wave.png'
+import Instagram from '@iconscout/react-unicons/icons/uil-instagram'
+import GitHub from '@iconscout/react-unicons/icons/uil-github'
+import Facebook from '@iconscout/react-unicons/icons/uil-facebook'
+
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <img src={wave} style={{width:'100%'}} />
+    <div className="footer" >
+      <img src={wave} alt="" style={{width:'100%'}}/>
       <div className="f-content">
-        
+        <span>abrhamzewdu2119@gmail.com</span>
+        <div>
+          <Instagram color='white' size='3rem'/>
+          <GitHub color='white' size='3rem'/>
+          <Facebook color='white' size='3rem'/>
+        </div>
       </div>
     </div>
+
   )
 }
 
